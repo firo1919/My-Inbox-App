93b6180e5d5a00311504389d426ae6ffdff34a34
firomsa <firomsassf@gmail.com>
2024-07-24 12:22:10 +0300

    added correct application.properties

---
+++ src/main/java/com/firomsa/MyInboxApp/CassandraConfig.java
@@ -0,0 +1,23 @@
+package com.firomsa.MyInboxApp;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories
+public class CassandraConfig {
+
+    @Value("${secure-connect-bundle}")
+    private String secureConnectBundlePath;
+
+    @Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer() {
+		return builder -> builder.withCloudSecureConnectBundle(Paths.get(secureConnectBundlePath));
+	}
+}
--- src/main/java/com/firomsa/MyInboxApp/MyInboxApp.java
+++ src/main/java/com/firomsa/MyInboxApp/MyInboxApp.java
@@ -1,20 +1,12 @@
 package com.firomsa.MyInboxApp;
 
-import java.nio.file.Path;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class MyInboxApp {
-	public static void main(String[] args) {
-		SpringApplication.run(MyInboxApp.class, args);
-	}
-	@Bean
-	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
-		Path bundle = astraProperties.getSecureConnectBundle().toPath();
-		return builder -> builder.withCloudSecureConnectBundle(bundle);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(MyInboxApp.class, args);
+    }
 }
